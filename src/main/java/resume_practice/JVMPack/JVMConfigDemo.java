d0ad052e8c71759d8904ae2e34ad374555d22871
wanyang <605968060@qq.com>
2020-02-24 17:12:15 +0800

    add jvm demo

---
+++ src/main/java/resume_practice/JVMPack/JVMConfigDemo.java
@@ -0,0 +1,23 @@
+package resume_practice.JVMPack;
+
+/**
+ * @title: demo
+ * @author: Young
+ * @desc: demo
+ * @date: Created at 2020/02/24 11:56
+ */
+public class JVMConfigDemo {
+
+    public static void main(String[] args) {
+        System.out.println("JVM config -----");
+        try {
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
